60f5d2daf1e33c115de874c3d02b5023aeec3464
smkw15 <samukawa15@gmail.com>
2025-05-20 01:09:09 +0900

    ファイルの体裁を整える。ファイルセパレータの追加、余白の設定。

--- constants.py
+++ constants.py
@@ -1,6 +1,28 @@
+# ディレクトリ名・ファイル名
 SRC_DIR_PATH = ".\\docs\\src"
 DEST_DIR_PATH = ".\\docs\\dest"
 DEST_FILE_NAME = "__all_in_one__.docx"
 
+# コンポーズ規則
+FILE_SEPARATOR = ['', "＊", '']  # default:['', "＊", '']
+
+# ドキュメントのスタイル
+PARAGRAPH_STYLE_NAME = 'Body Text'  # default:'Body Text'
+# 行間の幅
+PARAGRAPH_PT_BEFORE = 0  # default:0
+PARAGRAPH_PT_AFTER = 0  # default:0
+# ページサイズ
+PAGE_WIDTH_MM = 210  # default:210
+PAGE_HEIGHT_MM = 297  # default:297
+# ページ余白
+LEFT_MARGIN_MM = 19.0  # default:25.4
+TOP_MARGIN_MM = 19.0  # default:25.4
+RIGHT_MARGIN_MM = 19.0  # default:25.4
+BOTTOM_MARGIN_MM = 19.0  # default:25.4
+# ヘッダー/フッターの幅
+HEADER_DISTANCE_MM = 19.0  # default:12.7
+FOOTER_DISTANCE_MM = 19.0  # default:12.7
+
+# 一般規則
 SRC_NEWLINE = "\r\n"
 SRC_ENCODIG = "utf-8"
--- main.py
+++ main.py
@@ -3,44 +3,75 @@ import os
 import shutil
 from functools import reduce
 from docx import Document
+from docx.document import Document as DocumentObject
+from docx.shared import Pt, Mm
 from constants import (
-    SRC_DIR_PATH,
-    DEST_DIR_PATH,
-    DEST_FILE_NAME,
-    SRC_NEWLINE,
-    SRC_ENCODIG
+  SRC_DIR_PATH,
+  DEST_DIR_PATH,
+  DEST_FILE_NAME,
+  FILE_SEPARATOR,
+  PARAGRAPH_STYLE_NAME,
+  PARAGRAPH_PT_BEFORE,
+  PARAGRAPH_PT_AFTER,
+  PAGE_WIDTH_MM,
+  PAGE_HEIGHT_MM,
+  LEFT_MARGIN_MM,
+  TOP_MARGIN_MM,
+  RIGHT_MARGIN_MM,
+  BOTTOM_MARGIN_MM,
+  HEADER_DISTANCE_MM,
+  FOOTER_DISTANCE_MM,
+  SRC_NEWLINE,
+  SRC_ENCODIG,
 )
 
 def _find_txt_file(dir_path: str):
-    """txtファイル検索"""
-    file_pathes: list[str] = glob.glob(os.path.join(dir_path, "**", "*.txt"), recursive=True)
-    return sorted(file_pathes, key=os.path.basename)
+  """txtファイル検索"""
+  file_pathes: list[str] = glob.glob(os.path.join(dir_path, "**", "*.txt"), recursive=True)
+  return sorted(file_pathes, key=os.path.basename)
 
 def _read_txt_file(file_path: str) -> list[str]:
-    """txtファイル読み込み"""
-    with open(file_path, mode="r", encoding=SRC_ENCODIG, newline=SRC_NEWLINE) as f:
-        return f.readlines()
+  """txtファイル読み込み"""
+  with open(file_path, mode="r", encoding=SRC_ENCODIG, newline=SRC_NEWLINE) as f:
+    return f.read().strip(SRC_NEWLINE).split(SRC_NEWLINE)  # ファイルの先頭と末尾にある改行はトリム
 
-def _write_docx_file(dir_path: str, file_name: str, lines: list[str]):
-    """docxファイル書き込み"""
-    # 出力先の準備
-    if os.path.exists(dir_path):
-        shutil.rmtree(dir_path)  # 既に存在していたら削除
-    os.mkdir(dir_path)
-    # ファイル書き込み
-    doc = Document()
-    for line in lines:
-        doc.add_paragraph(line)
-    doc.save(os.path.join(dir_path, file_name))
+def _write_docx_file(dir_path: str, file_name: str, lines: list[str]) -> str:
+  """docxファイル書き込み"""
+  # 出力先の準備
+  if os.path.exists(dir_path):
+    shutil.rmtree(dir_path)  # 既に存在していたら削除
+  os.mkdir(dir_path)
+  # ドキュメント用意
+  doc = Document()
+  section = doc.sections[0]
+  section.page_width = Mm(PAGE_WIDTH_MM)
+  section.page_height = Mm(PAGE_HEIGHT_MM)
+  section.left_margin = Mm(LEFT_MARGIN_MM)
+  section.top_margin = Mm(TOP_MARGIN_MM)
+  section.right_margin = Mm(RIGHT_MARGIN_MM)
+  section.bottom_margin = Mm(BOTTOM_MARGIN_MM)
+  section.header_distance = Mm(HEADER_DISTANCE_MM)
+  section.footer_distance = Mm(FOOTER_DISTANCE_MM)
+  # ドキュメントに段落を追加
+  for line in lines:
+    paragraph = doc.add_paragraph(line, style=PARAGRAPH_STYLE_NAME)
+    paragraph.paragraph_format.space_before = Pt(PARAGRAPH_PT_BEFORE)
+    paragraph.paragraph_format.space_after = Pt(PARAGRAPH_PT_AFTER)
+  # ドキュメント書き込み
+  dest_file_path = os.path.join(dir_path, file_name)
+  doc.save(dest_file_path)
+  return dest_file_path
 
 def main():
-    """メイン処理"""
-    # 対象ファイルの検索
-    src_file_pathes = _find_txt_file(SRC_DIR_PATH)
-    # 対象ファイルの内容を集積
-    lines: list[str] = reduce(lambda lst, src_file_path:  lst + _read_txt_file(src_file_path), src_file_pathes, [])
-    # docxに書き込み
-    _write_docx_file(DEST_DIR_PATH, DEST_FILE_NAME, lines)
+  """メイン処理"""
+  # 対象ファイルの検索
+  src_file_pathes = _find_txt_file(SRC_DIR_PATH)
+  # 対象ファイルの内容を集積
+  lines: list[str] = reduce(lambda lst, src_file_path:  lst + FILE_SEPARATOR + _read_txt_file(src_file_path), src_file_pathes, list[str]())
+  lines = lines[len(FILE_SEPARATOR):]  # 最初に余計なファイルセパレータが混じるので消す
+  # docxに書き込み
+  dest_file_path = _write_docx_file(DEST_DIR_PATH, DEST_FILE_NAME, lines)
+  print("created:", dest_file_path)
 
 if __name__ == "__main__":
     main()
